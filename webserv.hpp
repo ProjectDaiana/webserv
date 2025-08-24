d45fc481984cb336996efa93ac2d6636d3deb86f
Lisamarie Treser <lisamarietreser@yahoo.de>
2025-08-24 19:23:12 +0200

    chore: define server and listen bindings structs

--- webserv.hpp
+++ webserv.hpp
@@ -6,7 +6,7 @@
 /*   By: ltreser <ltreser@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/23 17:26:54 by ltreser           #+#    #+#             */
-/*   Updated: 2025/08/24 16:50:05 by ltreser          ###   ########.fr       */
+/*   Updated: 2025/08/24 19:22:38 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,26 @@
 
 typedef struct s_arena t_arena;
 typedef struct s_data t_data;
+typedef struct s_listen_binding t_listen_binding;
+typedef struct s_server t_server;
+
+struct s_server
+{
+	const char			*name; //server name
+	t_listen_binding	*lb; //pointer to array of listen bindings
+	int					lb_count; //length of that array
+	const char			*error_pages; //array of file paths for error codes
+	int					*error codes; //array of status codes (eg 404)
+	size_t				max_bdy_size; //maximum allowed body size of the requests
+	t_location			**locations; //pointer to locations array
+	int					location_count; //length of that array
+}
+
+struct s_listen_binding
+{
+	char	*host;
+	int		port;
+};
 
 struct s_arena
 {
