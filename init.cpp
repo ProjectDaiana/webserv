51f0a2fd0a360bc8981fed890dcb73e512a92023
Lisamarie Treser <lisamarietreser@yahoo.de>
2025-08-30 20:00:23 +0200

    chore: allocate & hardcode parser

--- init.cpp
+++ init.cpp
@@ -6,7 +6,7 @@
 /*   By: ltreser <ltreser@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 16:34:29 by ltreser           #+#    #+#             */
-/*   Updated: 2025/08/29 16:09:57 by ltreser          ###   ########.fr       */
+/*   Updated: 2025/08/30 19:57:41 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,33 +27,40 @@ t_data *init_data(void)
 
 
 //allocating and hardcoding config for listen_binding, server, location
+//OJO only string literals, cant be changed later, but dont have to be
+//TODO create index and cgi files
 void	init_config(t_data *d, t_arena *mem)
 {
 	d->s = (t_server *)arena_alloc(mem, sizeof(t_server));
 	d->l = (t_location *)arena_alloc(mem, sizeof(t_location));
-	d->s->name = (char *)arena_alloc(mem, 11);
-	strcpy(d->s->name, "miniserver\n");
+	d->s->name = "miniserver";
 	d->s->lb_count = 1;
-	d->s->lb = (t_listen_binding **)arena_alloc(mem, d->s->lb_count * sizeof(t_listen_binding);
+	d->s->lb = (t_listen_binding **)arena_alloc(mem, d->s->lb_count * sizeof(t_listen_binding));
 	d->s->lb[0] = (t_listen_binding *)arena_alloc(mem, sizeof(t_listen_binding));
-	d->s->lb[0]->host = (char *)arena_alloc(mem, 8);
-	strcpy(d->s->lb[0]->host, "0.0.0.0\n");
+	d->s->lb[0]->host = "0.0.0.0";
 	d->s->lb[0]->port = 8080;
-	d->s->error_pages_count = 1;
-	d->s->error_pages = (char **)arena_alloc(mem, d->s->error_pages_count * sizeof(char *));
-	d->s->error_pages[0] = (char *)arena_alloc(mem, strlen("/errors/404.html" + 1);
-	strcpy(d->s->error_pages[0], "/errors/404.html");
-	d->s->error_codes_count = 1;
-	d->s->error_codes = (int *)arena_alloc(mem, d->s->error_codes_count * 4);
+	d->s->error_page_count = 1;
+	d->s->error_pages = (const char **)arena_alloc(mem, d->s->error_page_count * sizeof(const char *));
+	d->s->error_pages[0] = "/errors/404.html";
+	d->s->error_code_count = 1;
+	d->s->error_codes = (int *)arena_alloc(mem, d->s->error_code_count * sizeof(int));
 	d->s->error_codes[0] = 404;
 	d->s->max_bdy_size = 1 * 1024 * 1024; //1mb
 	d->s->location_count = 1;
-	d->s->locations = (t_location **)arena_alloc(mem, d->s->location_count * sizeof(t_location));
+	d->s->locations = (t_location **)arena_alloc(mem, d->s->location_count * sizeof(t_location *));
 	d->s->locations[0] = (t_location *)arena_alloc(mem, sizeof(t_location));
-	d->s->locations[0]->path = (char *)arena_alloc(mem, 8);
-	stcpy(d->s->locations[0]->path, "/images");
-	//TODO use str literals instead, bc read only makes mroe sense
-
-		
-
+	d->s->locations[0]->path = "/images";
+	d->s->locations[0]->method_count = 1;
+	d->s->locations[0]->accepted_methods = (const char **)arena_alloc(mem, d->s->locations[0]->method_count * sizeof(const char *));
+	d->s->locations[0]->accepted_methods[0] = "GET";
+	d->s->locations[0]->redirect = "https://cataas.com/cat";
+	d->s->locations[0]->root = "/var/www/html";
+	d->s->locations[0]->autoindex = 1; //OJO enabled
+	d->s->locations[0]->default_file = "index.html"; //TODO put index file
+	d->s->locations[0]->upload_enabled = 0; //OJO disabled
+	d->s->locations[0]->upload_store = "/var/www/html/uploads";
+	d->s->locations[0]->cgi_count = 1;
+	d->s->locations[0]->cgi_extensions = (const char **)arena_alloc(mem, d->s->locations[0]->cgi_count * sizeof (const char *));
+	d->s->locations[0]->cgi_extensions[0] = ".php";
+	d->s->locations[0]->cgi_path = "/usr/bin/php-cgi"; //TODO create later
 }
