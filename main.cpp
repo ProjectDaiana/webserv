b599b4fd376306f8896f4af60d5b1908e89fed57
Lisamarie Treser <lisamarietreser@yahoo.de>
2025-08-30 20:01:13 +0200

    chore: add init to main

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 /*   By: ltreser <ltreser@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/24 16:26:00 by ltreser           #+#    #+#             */
-/*   Updated: 2025/08/24 16:49:44 by ltreser          ###   ########.fr       */
+/*   Updated: 2025/08/30 19:30:35 by ltreser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@ int	main(void)
 {
 	t_data *data = init_data();
 	printf("data allocated\n");
+	init_data();
 	free_arena(data->perm_memory);
 	printf("data freed\n");
 	return (0);
