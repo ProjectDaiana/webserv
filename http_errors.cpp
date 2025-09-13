f9c6a4db90d745e3f06e90c22a5aec4bf3e1487f
Lima <me>
2025-09-13 19:29:00 +0200

    feat: map http errors to reason phrases

---
+++ http_errors.cpp
@@ -0,0 +1,43 @@
+#include "webserv.hpp"
+
+//list of error codes
+std::string get_reason_phrase(int code) 
+{
+    switch (code) 
+    {
+        // Success
+        case 200: return "OK";
+        case 201: return "Created";
+
+        // Redirection
+        case 300: return "Multiple Choices";
+        case 301: return "Moved Permanently";
+        case 302: return "Found";
+        case 303: return "See Other";
+        case 307: return "Temporary Redirect";
+        case 308: return "Permanent Redirect";
+
+        // Client errors
+        case 400: return "Bad Request";
+        case 401: return "Unauthorized";
+        case 403: return "Forbidden";
+        case 404: return "Not Found";
+        case 405: return "Method Not Allowed";
+        case 406: return "Not Acceptable";
+        case 408: return "Request Timeout";
+        case 411: return "Length Required";
+        case 413: return "Payload Too Large";
+        case 414: return "URI Too Long";
+        case 415: return "Unsupported Media Type";
+
+        // Server errors
+        case 500: return "Internal Server Error";
+        case 501: return "Not Implemented";
+        case 502: return "Bad Gateway";
+        case 503: return "Service Unavailable";
+        case 504: return "Gateway Timeout";
+
+        default:  return "Unknown";
+    }
+}
+
