7002858af7c0827bda56a9321dc6629f3f084171
Lisamarie Treser <lisamarietreser@yahoo.de>
2025-11-03 19:46:14 +0100

    feat: config autosettings

--- config.cpp
+++ config.cpp
@@ -41,6 +41,43 @@ std::string read_config(const char *path)
 	return (content);
 }
 
+void	autosettings(t_data *d)
+{
+	int i;
+	int j;
+
+	i = 0;
+	if (!d->server_count)
+		//ft_error("Error: no servers have been configured\n", 1);
+		;
+	while (i < d->server_count)
+	{
+		j = 0;
+		if (!d->s[i]->lb)
+			//ft_error("Error: server has no listen binding\n", 1);
+			;
+		if (!d->s[i]->max_bdy_size)
+			 d->s[i]->max_bdy_size = 10 * 1024 * 1024;
+		if (!d->s[i]->location_count)
+			//ft_error("Error: server has no locations\n", 1);
+			;
+		while (j < d->s[i]->location_count)
+		{
+			if (!d->s[i]->locations[j]->path || !d->s[i]->locations[j]->root)
+				//ft_error("Error: location without valid path", 1);
+				;
+			if (!d->s[i]->locations[j]->method_count)
+				//ft_error("Error: location must allow at least one method", 1);
+				;
+			if (!d->s[i]->locations[j]->autoindex && !d->s[i]->locations[j]->default_file)
+				//ft_error("Error: allow autoindexing or provide a default file", 1);
+				;
+			j++;
+		}
+		i++;
+	}
+}
+
 void	init_config(t_data *d, t_arena *mem)
 {
 	t_lexer		*lx;
@@ -52,5 +89,5 @@ void	init_config(t_data *d, t_arena *mem)
 	std::string config_content = read_config("config/webserv.conf");
 	lexer(lx, config_content, mem);
 	parser(d, ps, lx, mem);
-	//autosettings(d); //TODO give all the rest of the server/location variables values if they dont have that
+	autosettings(d); //TODO give all the rest of the server/location variables values if they dont have that
 }
