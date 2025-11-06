d29d86c74460fef92f695c841130c656d47597a0
ProjectDaiana <dairotchen@gmail.com>
2025-11-06 03:10:42 +0100

    chore: add printf error code to response

--- response.cpp
+++ response.cpp
@@ -71,8 +71,8 @@ void	handle_client_write(Client &client, const t_server &config)
 		<< response.body;
 	std::string str_response(sstr.str());
 		printf("\n_______________________________\n");
-		printf("finished response:\n");
-	//	printf("status code is: '%d'\n", response.status_code);
+		//printf("finished response:\n");
+		printf(CLR_YELLOW"status code is: '%d'\n" CLR_RESET, response.status_code);
 		written = write(client.get_fd(), str_response.c_str(), str_response.size());
         write(1, str_response.c_str(), str_response.size());
 		if (written == (int)str_response.size())
